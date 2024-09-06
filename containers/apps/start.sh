2f04233b366a7b6f3221a2da5a8d701c5bcc779c
Mellouli, Ismail (IntlAssoc) <ismail.mellouli@nist.gov>
2024-09-06 09:43:13 -0400

    start and stop scripts

--- containers/apps/start.sh
+++ containers/apps/start.sh
@@ -1,2 +1,5 @@
-docker-compose build
-docker-compose up
+./clean-all.sh
+
+cp /sites/www/certs/hit-dev.key reverse-proxy/certs/hit-dev.key
+cp /sites/www/certs/hit-dev.crt reverse-proxy/certs/hit-dev.crt
+docker-compose -f docker-compose.yml up --build  -d
--- containers/apps/stop.sh
+++ containers/apps/stop.sh
@@ -1,3 +1,1 @@
-docker stop  vocabulary-service  vocabulary-service-db
-docker rm  vocabulary-service  vocabulary-service-db
-docker rmi -f vocabulary-service vocabulary-service-db
+docker-compose --verbose down
